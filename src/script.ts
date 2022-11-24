e950c0b6437d7dc05685aa6a18150589c01d32f3
LeonardoRCosta <rm94286@fiap.com.br>
2022-11-24 19:26:52 -0300

    Script to convert all the files from the data directory.

---
+++ src/script.ts
@@ -0,0 +1,6 @@
+import CsvToJsonConversor from "./CsvToJsonConversor";
+const fs = require("fs");
+fs.readdirSync("data").map((file: string) => {
+  let csv2json = new CsvToJsonConversor(file, "data", file.replace(".csv", ""));
+  csv2json.writeJSON();
+});
