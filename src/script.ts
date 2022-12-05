3b8edd4758457c583cf750185ab97d5e4162d5c1
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-05 18:49:07 -0300

    refactor: switched .map to .forEach

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,6 @@
 import CsvToJsonConversor from "./CsvToJSON";
 const fs = require("fs");
-fs.readdirSync("data").map((file: string) => {
+fs.readdirSync("data").forEach((file: string) => {
   let csv2json = new CsvToJsonConversor(file, "data", file.replace(".csv", ""));
   csv2json.writeJSON();
 });
